3e7cdc1ea2e74ecf44ad35d71b8d030e74d55abf
erikwett <erik.wetterberg@gmail.com>
2019-09-10 10:53:58 +0200

    Build

---
+++ nova-table-ext/extDefinition.js
@@ -0,0 +1,2 @@
+define(function(){"use strict";return{definition:{type:"items",component:"accordion",items:{data:{uses:"data"},settings:{uses:"settings"}}},support:{export:!0,exportData:!0,snapshot:!0,viewData:!1}}});
+//# sourceMappingURL=extDefinition.js.map
